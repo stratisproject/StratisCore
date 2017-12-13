74d1a409a6040022abaddbeebd0d9e26d2c3bceb
dev0tion <dev0tion@hotmail.com>
2017-12-13 16:22:41 +0100

    Add wallet name in menu

--- FullNode.UI/src/app/wallet/menu/menu.component.ts
+++ FullNode.UI/src/app/wallet/menu/menu.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { LogoutConfirmationComponent } from '../logout-confirmation/logout-confirmation.component';
 
 import { ModalService } from '../../shared/services/modal.service';
+import { GlobalService } from '../../shared/services/global.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {
-  constructor(private modalService: NgbModal) {}
+export class MenuComponent implements OnInit {
+  constructor(private modalService: NgbModal, private globalService: GlobalService) {}
+  public walletName: string;
+
+  ngOnInit() {
+    this.walletName = this.globalService.getWalletName();
+  }
 
   public logOut() {
     const modalRef = this.modalService.open(LogoutConfirmationComponent);
