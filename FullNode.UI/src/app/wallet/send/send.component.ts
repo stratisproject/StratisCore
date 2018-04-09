8faa6e5fd6faed2f0d1b95f3c98ef6343e93e78c
dev0tion <dev0tion@hotmail.com>
2018-04-09 15:09:55 +0200

    Check if send amount doesn't exceed available balance

--- FullNode.UI/src/app/wallet/send/send.component.ts
+++ FullNode.UI/src/app/wallet/send/send.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 
 import { ApiService } from '../../shared/services/api.service';
 import { GlobalService } from '../../shared/services/global.service';
@@ -11,8 +11,11 @@ import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FeeEstimation } from '../../shared/classes/fee-estimation';
 import { TransactionBuilding } from '../../shared/classes/transaction-building';
 import { TransactionSending } from '../../shared/classes/transaction-sending';
+import { WalletInfo } from '../../shared/classes/wallet-info';
 
 import { SendConfirmationComponent } from './send-confirmation/send-confirmation.component';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'send-component',
@@ -29,22 +32,28 @@ export class SendComponent implements OnInit {
   public coinUnit: string;
   public isSending: boolean = false;
   public estimatedFee: number = 0;
+  public totalBalance: number = 0;
   public apiError: string;
   private isMaxBalance: boolean = false;
   private transactionHex: string;
   private responseMessage: any;
   private errorMessage: string;
   private transaction: TransactionBuilding;
+  private walletBalanceSubscription: Subscription;
 
   ngOnInit() {
+    this.startSubscriptions();
     this.coinUnit = this.globalService.getCoinUnit();
   }
+
+  ngOnDestroy() {
+    this.cancelSubscriptions();
+  };
 
   private buildSendForm(): void {
     this.sendForm = this.fb.group({
       "address": ["", Validators.compose([Validators.required, Validators.minLength(26)])],
-      // "amount": ["", Validators.compose([Validators.required, Validators.pattern(/^[0-9]+(\.[0-9]{0,8})?$/)])],
-      "amount": ["", Validators.compose([Validators.required, Validators.pattern(/^([0-9]+)?(\.[0-9]{0,8})?$/), Validators.min(0.00001)])],
+      "amount": ["", Validators.compose([Validators.required, Validators.pattern(/^([0-9]+)?(\.[0-9]{0,8})?$/), Validators.min(0.00001), (control: AbstractControl) => Validators.max((this.totalBalance - this.estimatedFee)/100000000)(control)])],
       "fee": ["medium", Validators.required],
       "password": ["", Validators.required]
     });
@@ -91,7 +100,8 @@ export class SendComponent implements OnInit {
     'amount': {
       'required': 'An amount is required.',
       'pattern': 'Enter a valid transaction amount. Only positive numbers and no more than 8 decimals are allowed.',
-      'min': "The amount has to be more or equal to 0.00001 Stratis."
+      'min': "The amount has to be more or equal to 0.00001 Stratis.",
+      'max': 'The total transaction amount exceeds your available balance.'
     },
     'fee': {
       'required': 'A fee is required.'
@@ -267,9 +277,53 @@ export class SendComponent implements OnInit {
     ;
   }
 
+  private getWalletBalance() {
+    let walletInfo = new WalletInfo(this.globalService.getWalletName());
+    this.walletBalanceSubscription = this.apiService.getWalletBalance(walletInfo)
+      .subscribe(
+        response =>  {
+          if (response.status >= 200 && response.status < 400) {
+              let balanceResponse = response.json();
+              //TO DO - add account feature instead of using first entry in array
+              this.totalBalance = balanceResponse.balances[0].amountConfirmed + balanceResponse.balances[0].amountUnconfirmed;
+          }
+        },
+        error => {
+          console.log(error);
+          if (error.status === 0) {
+            this.cancelSubscriptions();
+            this.genericModalService.openModal(null, null);
+          } else if (error.status >= 400) {
+            if (!error.json().errors[0]) {
+              console.log(error);
+            }
+            else {
+              if (error.json().errors[0].description) {
+                this.genericModalService.openModal(null, error.json().errors[0].message);
+              } else {
+                this.cancelSubscriptions();
+                this.startSubscriptions();
+              }
+            }
+          }
+        }
+      )
+    ;
+  };
+
   private openConfirmationModal() {
     const modalRef = this.modalService.open(SendConfirmationComponent, { backdrop: "static" });
     modalRef.componentInstance.transaction = this.transaction;
     modalRef.componentInstance.transactionFee = this.estimatedFee;
+  }
+
+  private cancelSubscriptions() {
+    if (this.walletBalanceSubscription) {
+      this.walletBalanceSubscription.unsubscribe();
+    }
+  };
+
+  private startSubscriptions() {
+    this.getWalletBalance();
   }
 }
