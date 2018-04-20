89b94f1d02908f5dfa5466423daa4b73af038a1a
dev0tion <dev0tion@hotmail.com>
2018-04-20 15:51:33 +0100

    Fix copy pasting on OS X

--- FullNode.UI/main.ts
+++ FullNode.UI/main.ts
@@ -218,5 +218,22 @@ function writeLog(msg) {
 };
 
 function createMenu() {
+  var menuTemplate = [{
+    label: app.getName(),
+    submenu: [
+      { label: "About " + app.getName(), selector: "orderFrontStandardAboutPanel:" },
+      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+  ];
 
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
 };
