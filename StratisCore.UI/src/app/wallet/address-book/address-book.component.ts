c037fd9800154ba488c07229d3cfc57e29ae11d7
Paul Herbert <paul.herbert@stratisplatform.com>
2018-10-29 11:12:37 +0000

    Added address-book component (+service).

--- StratisCore.UI/src/app/wallet/address-book/address-book.component.ts
+++ StratisCore.UI/src/app/wallet/address-book/address-book.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+
+import { AddressBookService } from './address-book-service';
+import { GlobalService } from '../../shared/services/global.service';
+
+export class Address {
+    constructor(public name: string, public address: string) { }
+}
 
 @Component({
-  selector: 'app-address-book',
-  templateUrl: './address-book.component.html',
-  styleUrls: ['./address-book.component.css']
+    selector: 'app-address-book',
+    templateUrl: './address-book.component.html',
+    styleUrls: ['./address-book.component.css']
 })
 export class AddressBookComponent implements OnInit {
 
-  constructor() { }
+    constructor(private globalService: GlobalService, private addressBookService: AddressBookService, private clipboardService: ClipboardService) { }
 
-  ngOnInit() {
-  }
+    addresses: Address[];
 
+    ngOnInit() {
+        this.addressBookService.GetAddresses(this.globalService.getWalletName())
+            .subscribe(x => this.addresses = x.map(a => new Address(a.name, a.address)));
+    }
+
+    copyToClipboardClicked(address: Address) {
+        if (this.clipboardService.copyFromContent(address.address)) {
+            console.log(address.name + ' ' + address.address);
+        }
+    }
+
+    sendClicked(address: Address) {
+        console.log(address.name);
+    }
 }
