9c22a7a3ff83699d7248823b11b6a9df35b8b3f7
Pieterjan Vanhoof <dev0tion@hotmail.com>
2018-12-19 08:05:27 +0100

    Add sidechain/smart contracts UI (#173)
    * Enable running the UI in sidechain mode
    
    * Update dashboard, logout, menu and status bar component for sidechains
    
    * Use node status to get coin ticker
    
    * Fix menu

--- StratisCore.UI/src/app/wallet/menu/menu.component.ts
+++ StratisCore.UI/src/app/wallet/menu/menu.component.ts
@@ -1,76 +1,38 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
 import { GlobalService } from '../../shared/services/global.service';
-import { FeaturesService } from '../../shared/services/features.service';
 import { LogoutConfirmationComponent } from '../logout-confirmation/logout-confirmation.component';
-
-type Option = { 'displayName': string, 'routerLink': string, 'isEnabled': boolean, 'featureName': string, 'icon': string }
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent implements OnInit, OnDestroy {
-    private featureSubscription: Subscription;
+export class MenuComponent implements OnInit {
+  constructor(private modalService: NgbModal, private globalService: GlobalService, private router: Router) {
+      this.walletName = this.globalService.getWalletName();
+  }
 
-    constructor(private modalService: NgbModal, private globalService: GlobalService, private featuresService: FeaturesService, private router: Router) {
+  public testnet: boolean;
+  public sidechainEnabled: boolean;
+  public walletName: string;
 
-        this.tabOptions = [
-            { 'displayName': 'Dashboard', 'routerLink': '/wallet', 'isEnabled': true, 'featureName': '', icon: '' },
-            { 'displayName': 'Cold Staking', 'routerLink': '/wallet/staking', 'isEnabled': this.coldStakingEnabled, 'featureName': 'coldstakingfeature', icon: '' },
-            { 'displayName': 'History', 'routerLink': '/wallet/history', 'isEnabled': true, 'featureName': '', icon: '' },
-            //{ 'displayName': 'Smart Contracts', 'routerLink': '/wallet/smart-contracts', 'isEnabled': true, 'featureName': '', icon: '' }
-        ];
+  ngOnInit() {
+    this.testnet = this.globalService.getTestnetEnabled();
+    this.sidechainEnabled = this.globalService.getSidechainEnabled();
+  }
 
-        this.dropdownOptions = [
-            { 'displayName': 'Advanced', 'routerLink': '/wallet/advanced', 'isEnabled': true, 'featureName': '', icon: 'lnr-cog'},
-            { 'displayName': 'Address Book', 'routerLink': '/wallet/address-book', 'isEnabled': true, 'featureName': '', icon: 'lnr-book' }
-        ];
+  openAddressBook() {
+    this.router.navigate(['/wallet/address-book']);
+  }
 
-        this.walletName = this.globalService.getWalletName();
+  openAdvanced() {
+    this.router.navigate(['/wallet/advanced']);
+  }
 
-        this.featureSubscription =
-            this.featuresService.getEnabledFeatures().subscribe(features => this.processEnabledFeatures(features));
-    }
-
-    walletName: string;
-    coldStakingEnabled = false;
-    tabOptions: Option[];
-    dropdownOptions: Option[];
-
-    logoutClicked() {
-        this.modalService.open(LogoutConfirmationComponent, { backdrop: "static" });
-    }
-
-    optionClicked(option: Option) {
-        if (option.routerLink) {
-            this.router.navigate([option.routerLink]);
-        }
-    }
-
-    ngOnInit() {
-      if (this.globalService.getNetwork() === "StratisMain") {
-        this.coldStakingEnabled = false;
-      } else {
-        this.coldStakingEnabled = true;
-      }
-    }
-
-    ngOnDestroy() {
-        this.featureSubscription.unsubscribe();
-    }
-
-    private processEnabledFeatures(features: string[]) {
-        // features.forEach(feature => {
-        //     const featureName = feature.toLowerCase();
-        //     const option = this.tabOptions.filter(o => o.featureName).find(o => o.displayName === featureName);
-        //     if (option) {
-        //         option.isEnabled = true;
-        //     }
-        // });
-    }
+  logoutClicked() {
+      this.modalService.open(LogoutConfirmationComponent, { backdrop: "static" });
+  }
 }
