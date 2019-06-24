670721e13941b673f8ed7c0f6d5159d6cb0af5c5
Tyler Pena <tylerpena@me.com>
2019-06-24 06:22:57 -0700

    Resolve issue #221 (#242)
    * open wallet directory link in advanced -> about component
    
    * refactor advanced -> about component -async sub in tmpl
    
    * remove console log
    
    * check if wallet is in browser or electron, adjust UI respectively

--- StratisCore.UI/src/app/wallet/advanced/components/about/about.component.ts
+++ StratisCore.UI/src/app/wallet/advanced/components/about/about.component.ts
@@ -1,55 +1,35 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ApiService } from '@shared/services/api.service';
-import { ModalService } from '@shared/services/modal.service';
 import { NodeStatus } from '@shared/models/node-status';
 import { GlobalService } from '@shared/services/global.service';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit, OnDestroy {
+export class AboutComponent implements OnInit {
+  nodeStatusSubscription$: Observable<NodeStatus>;
+  applicationVersion: string;
+  isElectron: boolean;
 
-  constructor(private globalService: GlobalService, private apiService: ApiService, private genericModalService: ModalService) { }
-
-  private nodeStatusSubscription: Subscription;
-  public clientName: string;
-  public applicationVersion: string;
-  public fullNodeVersion: string;
-  public network: string;
-  public protocolVersion: number;
-  public blockHeight: number;
-  public dataDirectory: string;
+  constructor(
+    private globalService: GlobalService,
+    private apiService: ApiService,
+    private electron: ElectronService
+  ) { }
 
   ngOnInit() {
+    this.isElectron = this.electron.isElectronApp;
     this.applicationVersion = this.globalService.getApplicationVersion();
-    this.startSubscriptions();
+    this.nodeStatusSubscription$ = this.apiService.getNodeStatusInterval();
   }
 
-  ngOnDestroy() {
-    this.cancelSubscriptions();
-  }
+  openWalletDirectory(directory: string): void {
+    if (!this.isElectron) return;
 
-  private startSubscriptions() {
-    this.nodeStatusSubscription = this.apiService.getNodeStatusInterval()
-      .subscribe(
-        (data: NodeStatus) =>  {
-          let statusResponse = data
-          this.clientName = statusResponse.agent;
-          this.fullNodeVersion = statusResponse.version;
-          this.network = statusResponse.network;
-          this.protocolVersion = statusResponse.protocolVersion;
-          this.blockHeight = statusResponse.blockStoreHeight;
-          this.dataDirectory = statusResponse.dataDirectoryPath;
-        }
-      );
-  }
-
-  private cancelSubscriptions() {
-    if(this.nodeStatusSubscription) {
-      this.nodeStatusSubscription.unsubscribe();
-    }
+    this.electron.shell.showItemInFolder(directory);
   }
 }
