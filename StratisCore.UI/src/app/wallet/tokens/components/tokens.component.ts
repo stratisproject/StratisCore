9b4d3564981f78dca87204c8d05219a748dbe2a5
fenix2222 <igor.goldobin@gmail.com>
2019-07-09 09:29:05 +1000

    Added sample of using disposable

--- StratisCore.UI/src/app/wallet/tokens/components/tokens.component.ts
+++ StratisCore.UI/src/app/wallet/tokens/components/tokens.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { Disposable } from '../models/disposable';
+import { Mixin } from '../models/mixin';
+import { Log } from '../services/logger.service';
+import { TokensService } from '../services/tokens.service';
 
 @Component({
   selector: 'app-tokens',
   templateUrl: './tokens.component.html',
   styleUrls: ['./tokens.component.css']
 })
-export class TokensComponent implements OnInit {
+@Mixin([Disposable])
+export class TokensComponent implements OnInit, OnDestroy, Disposable {
+  disposed$ = new ReplaySubject<boolean>();
+  dispose: () => void;
 
-  constructor() { }
+  constructor(private tokenService: TokensService) { }
 
   ngOnInit() {
+    this.tokenService
+      .GetAddresses('')
+      .pipe(takeUntil(this.disposed$))
+      .subscribe(data => Log.info(data));
+  }
+
+  ngOnDestroy() {
+    this.dispose();
   }
 
 }
