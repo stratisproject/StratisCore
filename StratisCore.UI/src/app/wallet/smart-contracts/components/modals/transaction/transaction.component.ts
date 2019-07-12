fa85e06c9f901d17b39ce0716eb6872444f6368f
fenix2222 <igor.goldobin@gmail.com>
2019-07-12 14:51:31 +1000

    Lint fix

--- StratisCore.UI/src/app/wallet/smart-contracts/components/modals/transaction/transaction.component.ts
+++ StratisCore.UI/src/app/wallet/smart-contracts/components/modals/transaction/transaction.component.ts
@@ -181,22 +181,22 @@ export class TransactionComponent implements OnInit {
     this.feeAmount = new FormControl(0.001, [Validators.required, amountValidator, Validators.min(0)]);
     // tslint:disable-next-line:max-line-length
     this.gasPrice = new FormControl(100, [Validators.required, integerValidator, Validators.pattern('^[+]?([0-9]{0,})*[.]?([0-9]{0,2})?$'), gasPriceTooLowValidator, gasPriceTooHighValidator, Validators.min(0)]);
-    
-    if (this.mode == Mode.Call) {
+
+    if (this.mode === Mode.Call) {
       // tslint:disable-next-line:max-line-length
       this.gasLimit = new FormControl(this.gasCallLimitMinimum, [Validators.required, integerValidator, Validators.pattern('^[+]?([0-9]{0,})*[.]?([0-9]{0,2})?$'), gasLimitValidator, gasLimitMaximumValidator, Validators.min(0)]);
     }
 
-    if (this.mode == Mode.Create) {
+    if (this.mode === Mode.Create) {
       // tslint:disable-next-line:max-line-length
       this.gasLimit = new FormControl(this.gasCreateLimitMinimum, [Validators.required, integerValidator, Validators.pattern('^[+]?([0-9]{0,})*[.]?([0-9]{0,2})?$'), gasLimitValidator, gasLimitMaximumValidator, Validators.min(0)]);
     }
 
-    if (this.mode == Mode.IssueToken) {
+    if (this.mode === Mode.IssueToken) {
       // tslint:disable-next-line:max-line-length
       this.gasLimit = new FormControl(this.gasCreateTokenLimitMinimum, [Validators.required, integerValidator, Validators.pattern('^[+]?([0-9]{0,})*[.]?([0-9]{0,2})?$'), gasLimitValidator, gasLimitMaximumValidator, Validators.min(0)]);
     }
-    
+
     this.methodName = new FormControl('', [Validators.required, Validators.nullValidator]);
     const contractCode = this.mode === Mode.IssueToken ? this.newTokenByteCode : '';
     // tslint:disable-next-line:max-line-length
