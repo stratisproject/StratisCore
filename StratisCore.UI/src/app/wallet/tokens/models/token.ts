6ed2cfb7c48f12bec5af77fe667a16e80716c5d2
Igor <igor.goldobin@gmail.com>
2019-07-15 20:36:28 +1000

    Lint fix

--- StratisCore.UI/src/app/wallet/tokens/models/token.ts
+++ StratisCore.UI/src/app/wallet/tokens/models/token.ts
@@ -12,7 +12,7 @@ export class SavedToken extends Token {
   constructor(ticker: string, address: string, balance: number) {
     super(ticker, address);
     this.balance = balance;
-  } 
+  }
 
   balance: number;
 }
