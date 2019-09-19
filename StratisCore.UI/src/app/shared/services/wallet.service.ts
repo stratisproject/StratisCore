181512f4cb3d01ebb0ed0b056afec35ba94ae5c7
Ian Davies <ian_a_davies@hotmail.com>
2019-09-19 18:58:06 +0100

    Add devtools flag for debugging production builds + don't get history while in IBD mode.

--- StratisCore.UI/src/app/shared/services/wallet.service.ts
+++ StratisCore.UI/src/app/shared/services/wallet.service.ts
@@ -28,6 +28,7 @@ export class WalletService extends RestApi {
   private walletUpdatedSubjects: { [walletName: string]: BehaviorSubject<WalletBalance> } = {};
   private walletHistorySubjects: { [walletName: string]: BehaviorSubject<TransactionsHistoryItem[]> } = {};
   private currentWallet: WalletInfo;
+  private ibdMode: boolean;
   public accountsEnabled: boolean;
 
   constructor(
@@ -57,6 +58,7 @@ export class WalletService extends RestApi {
 
     signalRService.registerOnMessageEventHandler<WalletInfoSignalREvent>(SignalREvents.WalletGeneralInfo,
       (message) => {
+        this.ibdMode = !message.isChainSynced;
         if (message.walletName === this.currentWallet.walletName) {
           const walletBalance = message.accountsBalances.find(acc => acc.accountName === `account ${this.currentWallet.account}`);
           this.updateWalletForCurrentAddress(walletBalance);
@@ -68,8 +70,12 @@ export class WalletService extends RestApi {
     // to show Staking Rewards, TODO : this needs a SignalR Event also
     signalRService.registerOnMessageEventHandler<BlockConnectedSignalREvent>(SignalREvents.BlockConnected,
       (message) => {
+        if (this.ibdMode) {
+          return;
+        }
+
         const walletSubject = this.getWalletSubject(this.currentWallet);
-        if (!this.globalService.getSidechainEnabled() || walletSubject.value.amountUnconfirmed > 0) {
+        if ((!this.globalService.getSidechainEnabled()) || walletSubject.value.amountUnconfirmed > 0) {
           this.refreshWalletHistory();
         }
       });
@@ -119,7 +125,7 @@ export class WalletService extends RestApi {
     }
   }
 
-  public getWalletHistory(data: WalletInfo): Observable<WalletHistory> {
+  private getWalletHistory(data: WalletInfo): Observable<WalletHistory> {
     let extra = null;
     if (this.accountsEnabled) {
       extra = {
