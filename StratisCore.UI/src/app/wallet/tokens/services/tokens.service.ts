4f3b0d9a1f5bd4e4ee01824a68fad019229b2e77
Rowan de Haas <rowandehaas@outlook.com>
2020-01-22 23:06:36 +1100

    Parse balances from API as bignumbers

--- StratisCore.UI/src/app/wallet/tokens/services/tokens.service.ts
+++ StratisCore.UI/src/app/wallet/tokens/services/tokens.service.ts
@@ -3,7 +3,8 @@ import { LocalExecutionResult } from '@shared/models/local-execution-result';
 import { ApiService } from '@shared/services/api.service';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import JSONBigNumber from 'json-bignumber';
+import BigNumber from 'bignumber.js';
 import { LocalCallRequest } from '../models/LocalCallRequest';
 import { Result, ResultStatus } from '../models/result';
 import { SavedToken, Token } from '../models/token';
@@ -69,8 +70,19 @@ export class TokensService {
   }
 
   GetTokenBalance(request: TokenBalanceRequest): Observable<number> {
-    return this.LocalCall(request).pipe(
-      map(localExecutionresult => localExecutionresult.return ? localExecutionresult.return : 0)
+    return this.apiService.localCallRaw(request).pipe(
+      map(rawText => {
+        return JSONBigNumber.parse(rawText, function (key, value) {
+          if (key == "return") {
+            if(BigNumber.isBigNumber(value)) {
+              return value.toFixed();
+            }
+          } else {
+            return value;
+          }
+        });
+      }),
+      map(localExecutionresult => localExecutionresult.return ? localExecutionresult.return : "0")
     );
   }
 
