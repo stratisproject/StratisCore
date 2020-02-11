396dc87e228a040a180a9647c91d48c03f55d37b
dev0tion <pieterjan.vanhoof@stratisplatform.com>
2020-02-11 16:35:40 +0100

    Alphabetize wallet list

--- StratisCore.UI/src/app/login/login.component.ts
+++ StratisCore.UI/src/app/login/login.component.ts
@@ -87,7 +87,7 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.subscriptions.push(this.walletService.getWalletNames()
       .subscribe(
         response => {
-          this.wallets = response.walletNames;
+          this.wallets = response.walletNames.sort((a, b) => a.toLowerCase() < b.toLowerCase() ? -1 : 1);
           if (this.wallets.length > 0) {
             this.hasWallet = true;
           } else {
