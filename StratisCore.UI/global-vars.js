da039bc8f68ed4def847a1428b86e75358ebc207
Ian Davies <ian_a_davies@hotmail.com>
2020-04-02 17:52:14 +0300

    Update tsconfig to add docker helper

--- StratisCore.UI/global-vars.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var StartupStatus;
-(function (StartupStatus) {
-    StartupStatus["Downloading"] = "DOWNLOADING";
-    StartupStatus["Starting"] = "STARTING";
-    StartupStatus["Error"] = "ERROR";
-})(StartupStatus = exports.StartupStatus || (exports.StartupStatus = {}));
-//# sourceMappingURL=global-vars.js.map
