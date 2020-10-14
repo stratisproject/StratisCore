9ea7f800e26c93f0e4c6c2b7a7d183f6a787ef82
dev0tion <pieterjan.vanhoof@stratisplatform.com>
2020-10-14 16:03:43 +0200

    Update version

--- StratisCore.UI/src/app/shared/services/global.service.ts
+++ StratisCore.UI/src/app/shared/services/global.service.ts
@@ -17,7 +17,7 @@ export class GlobalService {
     this.setDaemonIP();
   }
 
-  private applicationVersion = '2.0.0';
+  private applicationVersion = '2.1.0';
   private gitCommit = "";
   private testnet = false;
   private sidechain = false;
